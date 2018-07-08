ac04c899be7e72c4e1c1dd7f35da68c39d0244a9
Bernardo <bernardopereiraf@gmail.com>
2018-07-08 01:13:40 -0300

    Initial Commit

---
+++ Assets/Entities/Enemy/EnemyBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public GameObject projectile;
+    public float health = 150f;   
+    public float projectileSpeed = 5f;
+
+
+    void Update()
+    {
+        Vector3 startPosition = transform.position + new Vector3(0, -1, 0);
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            GameObject enemyBeam = Instantiate(projectile, startPosition, Quaternion.identity) as GameObject;
+            enemyBeam.GetComponent<Rigidbody2D>().velocity = new Vector3(0, -projectileSpeed, 0);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        Projectile missile = collider.gameObject.GetComponent<Projectile>();
+        if(missile)
+        {
+            health -= missile.GetDamage();
+            missile.Hit();
+            if (health <= 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+
+    public void LoadScene(string name)
+    {
+        Debug.Log("New level Load: " + name);
+        SceneManager.LoadScene(name);
+    }
+
+    public void LoadNextScene()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex + 1);
+    }
+
+    public void LoadStartScene()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        print("Quit requested");
+        Application.Quit();
+    }
+    public void BrickDestroyed()
+    {
+        LoadNextScene();
+    }
+
+}
